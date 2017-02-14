93d8f8aefc17bdffd900173fea84cff2d2610290
makagucci <makagucci@gmx.com>
2017-02-14 14:51:34 +0000

    On the road to pragmatism.

---
+++ web-scrape/gen_temp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+TSTAMP="$(date +%d)/$(date +%m)/$(date +%Y)"
+echo '#!/usr/bin/python' > $1
+echo '# Created by' $(whoami)'@'$(hostname) >> $1
+echo '# On:' $TSTAMP >> $1
+echo '# --- Preamble --- #' >> $1
+echo '# --- Declarations --- #' >> $1
+echo '# --- Functions --- #' >> $1
+echo 'def main ():' >> $1
+echo '# --- Main --- #' >> $1
+echo 'if __name__ == "__main__":' >> $1
+echo '  main()' >> $1
+
+
